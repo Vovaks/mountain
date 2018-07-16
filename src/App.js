4a3347f58169028e403ed24408dda1ae5780e3ca
Vovaks <rjabkovvladimir@gmail.com>
2018-07-16 23:20:53 +0300

    updated translation files and added Login/Registration forms

--- src/App.js
+++ src/App.js
@@ -1,48 +1,107 @@
 import React, { Component } from 'react';
-import Menu from 'containers/menu/index'
-import logo from './logo.svg';
+import {BrowserRouter, Route} from  'react-router-dom';
+import i18next from "i18next";
 
+import Header from 'components/header/index.js';
+import Menu from 'containers/menu/index.js';
+
+/*pages*/
+import Home from 'components/pages/home/index.js';
+import Tv from 'components/pages/tv/index.js';
+import Sales from 'components/pages/sales/index.js';
+import Weather from 'components/pages/wearther/index.js';
+import User from 'components/pages/user/index.js';
+import Signin from 'components/pages/signin/index.js';
+import Signup from 'components/pages/signup/index.js';
+
+/*actions*/
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            language:"en"
+        };
+    }
+
+    componentWillMount() {
+        this.setLanguage(this.state.language);
+    }
+
+    /**
+     * setLanguage
+     * @param language
+     * Now load from json
+     * You can load translation for every page or for all pages
+     * Read in https://www.i18next.com
+     */
+    setLanguage(language) {
+        i18next.init({
+            lng: language,
+            resources: {
+                "ru": {
+                    "translation":
+                        require('translate/ru.json')
+                },
+                "en": {
+                    "translation":
+                        require('translate/en.json')
+                },
+            },
+        });
+    }
+
+    /**
+     * OnChange for Language Selector
+     * @param event
+     */
+    handleChangeLanguage = (event) => {
+        this.setState({lng: event.target.value});
+        this.setLanguage(event.target.value);
+    };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <select
-                id="lang"
-                // onChange={this.handleChangeLanguage}
-                // value={this.state.value}
-                value='en'
-            >
-                <option value='en'>English</option>
-                <option value='ru'>Russian</option>
-            </select>
-          <h1 className="App-title">Welcome to React</h1>
+        <BrowserRouter>
+            <div className="App">
+                <select
+                    id="lang"
+                    onChange={this.handleChangeLanguage}
+                    value={this.state.value}
+                >
+                    <option value='en'>English</option>
+                    <option value='ru'>Russian</option>
+                </select>
 
-        </header>
-
-          {/*Menu*/}
-          <div className={'menu'}>
-              <Menu/>
-          </div>
-
-          {/*Block*/}
-          <div className={'block'}>
-              <div className="content">
-                  <p className="App-intro">
-                      {/*{i18next.t('test_message')}*/}
-                  </p>
-                  <p className="App-intro">
-                      To get started, edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  {/*<Route exact path="/" component={Main}/>*/}
-                  {/*<Route path="/weather" component={Weather}/>*/}
-              </div>
-          </div>
-      </div>
+                {/*Header*/}
+                <header className="App-header">
+                    <Header/>
+                </header>
+                {/*Menu*/}
+                <div className={'menu'}>
+                    <Menu/>
+                </div>
+                {/*Block*/}
+                <div className={'block'}>
+                    <div className="content">
+                        <div>
+                            <Route exact path='/' component={Home} />
+                            <Route path='/tv' component={Tv} />
+                            <Route path='/sales' component={Sales} />
+                            <Route path='/weather' component={Weather} />
+                            <Route path='/user' component={User} />
+                            <Route path='/signin' component={Signin} />
+                            <Route path='/signup' component={Signup} />
+                        </div>
+                        {/*<p className="App-intro">*/}
+                            {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+                        {/*</p>*/}
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,22 +5,15 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import {BrowserRouter, Route} from  'react-router-dom'
 import {Provider} from 'react-redux';
 
 import reducers from 'reducers/index.js';
-
-/*pages*/
-import Home from './node_modules/components/home/index.js';
-import User from './node_modules/components/user/index.js';
-import Menu from 'containers/menu/index'
+import App from "./App.js";
 
 /* styles*/
 import './styles/App.css';
 import './styles/index.css';
 import './styles/main.scss';
-
-import logo from './logo.svg';
 
 // import registerServiceWorker from './registerServiceWorker';
 
@@ -31,46 +24,7 @@ const store = createStore(reducers, composeWithDevTools(
 // const history = syncHistoryWithStore(browserHistory, store);
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
-
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <select
-                        id="lang"
-                        // onChange={this.handleChangeLanguage}
-                        // value={this.state.value}
-                    >
-                        <option value='en'>English</option>
-                        <option value='ru'>Russian</option>
-                    </select>
-                    <h1 className="App-title">Welcome to React</h1>
-
-                </header>
-
-                {/*Menu*/}
-                <div className={'menu'}>
-                    <Menu/>
-                </div>
-
-                {/*Block*/}
-                <div className={'block'}>
-                    <div className="content">
-                        <div>
-                            {/*<Route path='/' component={App} />*/}
-                            <Route exact path='/' component={Home} />
-                            <Route path='/user' component={User} />
-                        </div>
-                        <p className="App-intro">
-                            To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-                        {/*<Route exact path="/" component={Main}/>*/}
-                        {/*<Route path="/weather" component={Weather}/>*/}
-                    </div>
-                </div>
-            </div>
-        </BrowserRouter>
-
+        <App/>
     </Provider>,
     document.getElementById('root')
 );
