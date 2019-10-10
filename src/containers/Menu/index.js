f0048cc3facb8cba21e935d3368d236e4c65d52d
Vladimir Rjabkov <vladimir@alphablues.com>
2019-10-10 13:42:00 +0300

    new exp

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 
 class Menu extends Component {
   render() {
-    const asd = { "asd!!:": "asd" }
+    const asd = { "asd!!:": "as1d" }
     return (
       <div>
         <div>{asd}</div>
