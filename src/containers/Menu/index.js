2d2cabee657a326834be6f39ef3ae5aef57c599a
Vladimir Rjabkov <vladimir@alphablues.com>
2019-10-10 13:14:30 +0300

    add

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -2,9 +2,10 @@ import React, { Component } from "react"
 
 class Menu extends Component {
   render() {
-    const asd = { "asd!!:": "asd" }
-    console.log("asd", asd)
+    const asd = { "asd!!:": "asd" };
 
+
+    console.log("asd", asd)
     return (
       <div>
         <div> {asd}</div>
